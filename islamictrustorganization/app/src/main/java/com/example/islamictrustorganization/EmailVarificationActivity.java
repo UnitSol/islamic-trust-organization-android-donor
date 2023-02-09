92fe8e876e6d6b1d7297f841a00f54a817c328e7
AliChohan11 <109972462+AliChohan11@users.noreply.github.com>
2023-02-09 15:11:26 +0500

    email varification screen done design

--- islamictrustorganization/app/src/main/java/com/example/islamictrustorganization/EmailVarificationActivity.java
+++ islamictrustorganization/app/src/main/java/com/example/islamictrustorganization/EmailVarificationActivity.java
@@ -1,14 +1,32 @@
 package com.example.islamictrustorganization;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EmailVarificationActivity extends AppCompatActivity {
-
+    EditText inputOne , inputTwo ,inputThree , inputFour;
+    TextView counterTimeText , textSignInCode;
+    Button verifyBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email_varification);
+        initalizing();
+    }
+
+    private void initalizing() {
+        inputOne = findViewById(R.id.inputOne);
+        inputTwo = findViewById(R.id.inputTwo);
+        inputThree = findViewById(R.id.inputThree);
+        inputFour = findViewById(R.id.inputFour);
+
+        counterTimeText = findViewById(R.id.counterTimeText);
+        textSignInCode = findViewById(R.id.textSignInCode);
+
+        verifyBtn = findViewById(R.id.verifyBtn);
     }
 }
