7e3ea6326047d01325fb803f632b4504967ac231
AliChohan11 <109972462+AliChohan11@users.noreply.github.com>
2023-02-09 19:06:47 +0500

    done

--- islamictrustorganization/app/src/main/java/com/example/islamictrustorganization/NewProjectActivity.java
+++ islamictrustorganization/app/src/main/java/com/example/islamictrustorganization/NewProjectActivity.java
@@ -1,14 +1,23 @@
 package com.example.islamictrustorganization;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NewProjectActivity extends AppCompatActivity {
-
+    ImageView backThroughBtn;
+    ListView newProjectItemsList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_project);
+        initalizing();
+    }
+
+    private void initalizing() {
+        backThroughBtn = findViewById(R.id.backThroughBtn);
+        newProjectItemsList = findViewById(R.id.newProjectItemsList);
     }
 }
