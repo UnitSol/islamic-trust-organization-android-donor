33dd0aa2fced77246db6d88819fd392ccf5b14e3
AliChohan11 <109972462+AliChohan11@users.noreply.github.com>
2023-02-09 19:04:20 +0500

    new project activity Design done

---
+++ islamictrustorganization/app/src/main/java/com/example/islamictrustorganization/NewProjectActivity.java
@@ -0,0 +1,14 @@
+package com.example.islamictrustorganization;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewProjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_project);
+    }
+}
