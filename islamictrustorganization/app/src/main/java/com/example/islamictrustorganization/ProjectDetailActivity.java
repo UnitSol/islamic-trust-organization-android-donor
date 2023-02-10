20c9f292c32ace92253ab15b89a7e1a2957f22c3
AliChohan11 <109972462+AliChohan11@users.noreply.github.com>
2023-02-10 18:18:56 +0500

    design done

--- islamictrustorganization/app/src/main/java/com/example/islamictrustorganization/ProjectDetailActivity.java
+++ islamictrustorganization/app/src/main/java/com/example/islamictrustorganization/ProjectDetailActivity.java
@@ -2,13 +2,15 @@ package com.example.islamictrustorganization;
 
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProjectDetailActivity extends AppCompatActivity {
-    TextView projectName , textStartDate , textEndDate , projectPrize ,textStatus;
+    TextView projectName , textStartDate , textEndDate , projectPrize ,textStatus , textDescription , textReadMore;
     ImageView projectImage , goBackBtn;
+    ListView updateListItem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +24,12 @@ public class ProjectDetailActivity extends AppCompatActivity {
         textEndDate = findViewById(R.id.textEndDate);
         projectPrize = findViewById(R.id.projectPrize);
         textStatus = findViewById(R.id.textStatus);
+        textDescription = findViewById(R.id.textDescription);
+        textReadMore = findViewById(R.id.textReadMore);
 
         projectImage = findViewById(R.id.projectImage);
         goBackBtn = findViewById(R.id.goBackBtn);
+
+        updateListItem = findViewById(R.id.updateListItem);
     }
 }
---
+++ islamictrustorganization/app/src/main/java/com/example/islamictrustorganization/ProjectDetailStepOneActivity.java
@@ -0,0 +1,14 @@
+package com.example.islamictrustorganization;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProjectDetailStepOneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_detail_step_one);
+    }
+}
