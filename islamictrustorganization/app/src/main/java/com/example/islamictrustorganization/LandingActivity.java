b7d92205494ce89635026f9f58ba572cbfca7e48
AliChohan11 <109972462+AliChohan11@users.noreply.github.com>
2023-02-13 16:12:09 +0500

    nav

--- islamictrustorganization/app/src/main/java/com/example/islamictrustorganization/LandingActivity.java
+++ islamictrustorganization/app/src/main/java/com/example/islamictrustorganization/LandingActivity.java
@@ -1,5 +1,6 @@
 package com.example.islamictrustorganization;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -7,7 +8,10 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class LandingActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
     ImageView userPicture;
     TextView userName , textDollarWallet , textCompletedProject , textInProgressProject;
     ListView projectList;
@@ -16,6 +20,7 @@ public class LandingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
         initalizing();
+
     }
 
     private void initalizing() {
@@ -27,5 +32,34 @@ public class LandingActivity extends AppCompatActivity {
         textInProgressProject = findViewById(R.id.textInProgressProject);
 
         projectList = findViewById(R.id.projectList);
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setSelectedItemId(R.id.menu_donner_dashboard);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.menu_donner_dashboard:
+                    startActivity(new Intent(getApplicationContext(), LandingActivity.class));
+                    overridePendingTransition(0, 0);
+                    finish();
+                    return true;
+                case R.id.menu_donner_new_projects:
+                    startActivity(new Intent(getApplicationContext(), NewProjectActivity.class));
+                    overridePendingTransition(0, 0);
+                    finish();
+                    return true;
+//                case R.id.menu_buyer_order_piece:
+//                    startActivity(new Intent(getApplicationContext(), BuyerOrderPieceActivity.class));
+//                    overridePendingTransition(0, 0);
+//                    finish();
+//                    return true;
+//                case R.id.menu_buyer_orders:
+//                    startActivity(new Intent(getApplicationContext(), BuyerOrdersActivity.class));
+//                    overridePendingTransition(0, 0);
+//                    finish();
+//                    return true;
+//                case R.id.menu_buyer_more:
+//                    return true;
+            }
+            return false;
+        });
     }
 }
